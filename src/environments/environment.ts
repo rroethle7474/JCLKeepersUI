bbdad5a906f0e17c5aa39bd65bcaab2bbe79eb4e
Ryan Roethle <ryanroethle7474@gmail.com>
2025-01-27 16:38:20 -0600

    added additional files and readme.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: false,
   apiUrl: 'http://127.0.0.1:8000/',
-  playerData: 'playerdata/?' //  player data endpoint using name and position (hitter or pitcher)
+  playerData: 'playerdata/?',  //  player data endpoint using name and position (hitter or pitcher)
+  openAIKey: 'test',
+  voterScraperUrl: '',
+
 };
